ea9eb172b6ca9c3cfd62e3c2b63e597d0678b01d
parkmm14 <palswo@naver.com>
2020-11-30 16:00:18 +0900

    @ApiOperation groupPoint컨트롤러랑 point컨트롤러에 추가

--- src/main/java/com/point/api/web/PointController.java
+++ src/main/java/com/point/api/web/PointController.java
@@ -8,6 +8,7 @@ import com.point.api.repository.PointRepository;
 import com.point.api.web.dto.*;
 import com.point.api.web.message.ErrorMessage;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
@@ -71,7 +72,7 @@ public class PointController {
 
 
 
-
+    @ApiOperation(value = "개인 점수 추가, 개인 점수 누적", notes = "점수 부여 기준에 맞게 산정된 점수를 point 테이블에 추가, 기존점수+산정된 점수 ranks 테이블에 추가")
     @PostMapping("/addPoint")
     public PointDto giveApoint(@RequestBody PointAddDto pointAddDto) {
         Point point = pointAddDto.toDomain();
@@ -106,6 +107,7 @@ public class PointController {
 
     //-----수정
     //유저의 전체 점수 이력을 조회
+    @ApiOperation(value = "전체 개인 점수 이력 조회", notes = "유저의 전체 개인 점수 이력을 조회")
     @GetMapping("/pointList/{accountId}")
     public List<PointDto> getUserAllPointList (@PathVariable String accountId) {
         List<Point> pointList = pointService.getUserAllPoint(accountId);
@@ -113,6 +115,7 @@ public class PointController {
     }
 
     //유저의 전체 누적 점수를 조회
+    @ApiOperation(value = "전체 개인 누적 점수 조회 ", notes = "유저의 전체 개인 점수 이력에서 모든 점수 합산")
     @GetMapping("/{accountId}")
     public int getUserAllPoint(@PathVariable String accountId) {
         System.out.println(accountId);
@@ -128,6 +131,7 @@ public class PointController {
 
     //특정날짜의 유저 누적 점수를 조회
     //------
+    @ApiOperation(value = "현재 유저 누적 점수를 조회")
     @PostMapping("/date")
     public int getUserPointByDate (@RequestBody PointDto pointDto) {
         String accountId = pointDto.getAccountId();
@@ -152,6 +156,7 @@ public class PointController {
     *   LocalDateTime endTime;
     * }
     */
+    @ApiOperation(value = "특정 날짜의 유저 총 점수 조회")
     @PostMapping("/dateterm")
     public int getTimeTermAllPoint(@RequestBody TimeTermDto timeTermDto){
         String accountId = timeTermDto.getAccountId();
@@ -171,20 +176,18 @@ public class PointController {
 
 
     //유저 점수 부여 취소
+    @ApiOperation(value = "유저 점수 부여 취소", notes = "accountId와 todoId에 해당되는 점수 취소")
     @DeleteMapping("/cancel/{accountId}/{todoId}")
     public void cancelPoint (@PathVariable String accountId, @PathVariable String todoId) {
         pointService.cancelPoint(accountId, todoId);
-
     }
 
     //전체 유저 랭킹 리스트 조회 (여기서 리턴되는 랭킹 리스트로 프론트에서 순위 표시하기(일단은))
+    @ApiOperation(value = "전체 유저 랭킹 리스트 조회", notes = "전체 유저의 전체 랭킹 리스트를 조회한다. 리턴된 유저 랭킹 리스트에서 인덱스로 순위 표시 프론트에서 함.")
     @GetMapping("/all/ranking")
     public List<RankDto> getAllAccountRanking(){
         return rankService.getUserAllRanking().stream().map(rank-> new RankDto(rank)).collect(Collectors.toList());
     }
-
-
-
 
 
 
